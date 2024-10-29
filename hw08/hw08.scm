eff257b8b81dda38fe6b4736c335aac03e145081
Edge <edge27452696803@gmail.com>
2024-10-29 00:59:57 +0000

    hw08

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -1,7 +1,37 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+    (if
+        (null? s)
+        #t
+        (if 
+            (null? (cdr s))
+            #t
+            (if
+                (> (car s) (car (cdr s)))
+                #f
+                (ascending? (cdr s))))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    (cond 
+        ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (if 
+        (or 
+            (null? lst1) 
+            (null? lst2))
+        (append lst1 lst2)
+        (cons 
+            (car lst1)
+            (cons
+                (car lst2)
+                (interleave (cdr lst1) (cdr lst2))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) 
+    (if 
+        (null? s)
+        nil
+        (append 
+            (list(car s))
+            (no-repeats (filter (lambda (x) (not (= x (car s)))) (cdr s))))))
