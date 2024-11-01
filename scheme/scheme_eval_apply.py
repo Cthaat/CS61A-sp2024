dcea3839fa3205c6e6af604cb11b46b7c96b187a
Edge <edge27452696803@gmail.com>
2024-11-01 08:39:47 +0000

    Scheme Problem 02

--- scheme/scheme_eval_apply.py
+++ scheme/scheme_eval_apply.py
@@ -45,10 +45,18 @@ def scheme_apply(procedure, args, env):
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
         "*** YOUR CODE HERE ***"
+        args_list = []
+        pos = args
+        while pos is not nil:
+            args_list.append(pos.first)
+            pos = pos.rest
+        if procedure.need_env is True:
+            args_list.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
             "*** YOUR CODE HERE ***"
+            return procedure.py_func(*args_list)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
